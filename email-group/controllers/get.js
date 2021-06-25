5a6591a6bb58b1d4369d212baabdaa6d0d106e54
Abhishek Rathore <abhirathore@565gmail.com>
2021-06-26 00:16:15 +0530

    Fixed a bug to now find email groups by owner's id.

--- email-group/controllers/get.js
+++ email-group/controllers/get.js
@@ -2,14 +2,14 @@ const EmailGroup = require('./../model');
 
 const getEmailGroup = async (req, res) => {
 	try {
-		const emailGroupId = req.params.id;
+		const ownerId = req.params.id;
 
-		const emailGroup = await EmailGroup.findById(emailGroupId);
-		if (!emailGroup) {
-			return res.status(400).json({ error: 'No such email group found.' });
+		const emailGroups = await EmailGroup.find({ owner: ownerId });
+		if (!emailGroups) {
+			return res.status(400).json({ error: 'Owner has no email groups.' });
 		}
 
-		return res.status(200).json(emailGroup);
+		return res.status(200).json(emailGroups);
 	} catch (error) {
 		return res.status(400).json({ error: error.message });
 	}
--- email-group/model.js
+++ email-group/model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const emailGroupSchema = mongoose.Schema({
 	owner: {
-		type: ObjectId,
+		type: mongoose.ObjectId,
 		required: true,
 	},
 	groupName: {
