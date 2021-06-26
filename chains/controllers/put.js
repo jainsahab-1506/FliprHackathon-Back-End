68b9750ed8ed89c97af09888b7e0bdd7df782f2c
Abhishek Rathore <abhirathore@565gmail.com>
2021-06-26 15:35:47 +0530

    Added functionality to edit chains

--- chains/controllers/put.js
+++ chains/controllers/put.js
@@ -1,70 +1,92 @@
-const { Chain, Messages } = require("./../model");
-const mongoose = require("mongoose");
-const { userSchema, tokenSchema } = require("../../model");
-const User = new mongoose.model("User", userSchema);
-const Token = new mongoose.model("Token", tokenSchema);
-const editchain = (req, res) => {
-  try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader.startsWith("Bearer ")) {
-      return res.status(400).json({
-        error: "Invalid request headers.",
-      });
-    }
+const mongoose = require('mongoose');
 
-    const tokenData = authHeader.split(" ")[1];
-    if (!tokenData) {
-      return res.status(400).json({
-        error: "Invalid token.",
-      });
-    }
+const { Chain } = require('./../model');
+const { Messages } = require('./../../messages/models');
+const { userSchema, tokenSchema } = require('../../model');
 
-    Token.findOne({ token: tokenData }, function (err, token) {
-      if (!token) {
-        return res.status(400).json({
-          error: "Invalid token.",
-        });
-      } else {
-        const tokenOwner = token.userid;
-        const ownerId = req.params.ownerid;
-        if (tokenOwner.toString() !== ownerId.toString()) {
-          console.log("Expected:", tokenOwner);
-          console.log("Found:", ownerId);
+const User = new mongoose.model('User', userSchema);
+const Token = new mongoose.model('Token', tokenSchema);
 
-          return res.status(400).json({
-            error: "Unauthorized request.",
-          });
-        } else {
-          const id = req.params.id;
-          const chaindata = {
-            chainname: req.body.chainname,
-            userid: req.body.userid,
-            emailgroupid: req.body.emailgroupid,
-            message: req.body.message,
-            frequency: req.body.frequency,
-            status: req.body.status,
-          };
-          Chain.findOneAndUpdate(
-            { _id: id },
-            chaindata,
-            { new: true, omitUndefined: true, runValidators: true },
-            function (err, updatedchain) {
-              if (err) {
-                return res.status(400).json({
-                  error: "Cannot Update Chain",
-                });
-              } else {
-                return res
-                  .status(200)
-                  .json({ success: "Chain Updated.", updatedchain });
-              }
-            }
-          );
-        }
-      }
-    });
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
+const authorizeUpdate = (req, res, next) => {
+	try {
+		const authHeader = req.headers.authorization;
+		if (!authHeader.startsWith('Bearer ')) {
+			return res.status(400).json({
+				error: 'Invalid request headers.',
+			});
+		}
+
+		const tokenData = authHeader.split(' ')[1];
+		if (!tokenData) {
+			return res.status(400).json({
+				error: 'Invalid token.',
+			});
+		}
+
+		Token.findOne({ token: tokenData }, function (err, token) {
+			if (!token) {
+				return res.status(400).json({
+					error: 'Invalid token.',
+				});
+			} else {
+				const tokenOwner = token.userid;
+				const ownerId = req.params.ownerid;
+				if (tokenOwner.toString() !== ownerId.toString()) {
+					console.log('Expected:', tokenOwner);
+					console.log('Found:', ownerId);
+
+					return res.status(400).json({
+						error: 'Unauthorized request.',
+					});
+				} else {
+					const id = req.params.id;
+					const chaindata = {
+						chainname: req.body.chainname,
+						userid: req.body.userid,
+						emailgroupid: req.body.emailgroupid,
+						message: req.body.message,
+						frequency: req.body.frequency,
+						status: req.body.status,
+					};
+					Chain.findOneAndUpdate(
+						{ _id: id },
+						chaindata,
+						{ new: true, omitUndefined: true, runValidators: true },
+						function (err, updatedchain) {
+							if (err) {
+								return res.status(400).json({
+									error: 'Cannot Update Chain',
+								});
+							} else {
+								next();
+							}
+						}
+					);
+				}
+			}
+		});
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
 };
-module.exports = editchain;
+
+const editchain = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const chain = await Chain.findById(id);
+
+		const messageId = chain.messageid;
+		const message = await Message.findOneAndUpdate(
+			{ _id: messageId },
+			{ text: req.body.message, attachments: req.files },
+			{ new: true, omitUndefined: true, runValidators: true }
+		);
+
+		return res.status(200).json({ success: 'Chain updated.', chain });
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
+};
+
+module.exports.editchain = editchain;
+module.exports.authorizeUpdate = authorizeUpdate;
--- chains/routes.js
+++ chains/routes.js
@@ -5,12 +5,12 @@ const upload = multer({ dest: 'uploads/' });
 
 const { createchain, authorizeRequest } = require('./controllers/post.js');
 const deletechain = require('./controllers/delete.js');
-const editchain = require('./controllers/put.js');
+const { editchain, authorizeUpdate } = require('./controllers/put.js');
 const getchains = require('./controllers/get.js');
 
 router.delete('/:id/:ownerid', deletechain);
 router.get('/:id', getchains);
 router.post('/:ownerid', authorizeRequest, upload.array('files'), createchain);
-router.put('/:id/:ownerid', editchain);
+router.put('/:id/:ownerid', authorizeUpdate, upload.array('files'), editchain);
 
 module.exports = router;
