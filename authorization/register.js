34c44cee69d81777983cbf03f21b99f2338f9202
Naman Jain <2019UCP1390@mnit.ac.in>
2021-06-27 13:02:05 +0530

    oauth

--- authorization/register.js
+++ authorization/register.js
@@ -24,7 +24,6 @@ passport.deserializeUser(function (id, done) {
 });
 
 const register = (req, res) => {
-  console.log(req.body);
   User.register(
     {
       username: req.body.username,
@@ -38,50 +37,38 @@ const register = (req, res) => {
         return res.status(400).json({ error: "Internal Server Error " });
       } else {
         passport.authenticate("local")(req, res, function () {
-          User.findOne(
-            { username: req.body.username },
-            function (err, userinfo) {
-              if (!err) {
-                const token = jwt.sign(
-                  {
-                    userId: userinfo.username,
-                  },
-                  process.env.SECRET
-                );
-                var tokendata = new Token({
-                  token: token,
-                  userid: userinfo._id,
-                });
-
-                tokendata.save(function (err, auth) {
-                  if (err) {
-                    Token.findOne(
-                      { userid: userinfo._id },
-                      function (error, resp) {
-                        if (resp) {
-                          return res
-                            .status(400)
-                            .json({ error: "User Already Logged In " });
-                        } else {
-                          return res
-                            .status(400)
-                            .json({ error: "Internal Server Error " });
-                        }
-                      }
-                    );
-                  } else {
-                    return res.status(200).json({
-                      success: "Logged In Successfully.",
-                      token: auth.token,
-                      profile: userinfo,
-                    });
-                  }
-                });
-              } else {
-                return res.status(400).json({ error: "Email Exists " });
-              }
-            }
+          const token = jwt.sign(
+            {
+              userId: userinfo.username,
+            },
+            process.env.SECRET
           );
+          var tokendata = new Token({
+            token: token,
+            userid: userinfo._id,
+          });
+
+          tokendata.save(function (err, auth) {
+            if (err) {
+              Token.findOne({ userid: userinfo._id }, function (error, resp) {
+                if (resp) {
+                  return res
+                    .status(400)
+                    .json({ error: "User Already Logged In " });
+                } else {
+                  return res
+                    .status(400)
+                    .json({ error: "Internal Server Error " });
+                }
+              });
+            } else {
+              return res.status(200).json({
+                success: "Logged In Successfully.",
+                token: auth.token,
+                profile: userinfo,
+              });
+            }
+          });
         });
       }
     }
