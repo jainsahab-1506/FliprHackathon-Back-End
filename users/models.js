7885d2d28a7941fcca7574489c344769fd9e0610
jainsahab-1506 <76787092+jainsahab-1506@users.noreply.github.com>
2021-07-08 21:23:09 +0530

    Update models.js

--- users/models.js
+++ users/models.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const credentialsSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
+
   email: {
     type: String,
     required: true,
